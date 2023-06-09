b5b1efe8a99f3bd5ba3aaa0c10dd257ce9cf8605
shashu22 <challasaisashank@gmail.com>
2023-06-09 11:45:30 -0500

    rotated

---
+++ 06-07-2023/Substring.java
@@ -0,0 +1,20 @@
+public class Substring {
+    public static void main(String[] args) {
+        String str = "JavaJ2eeStrutsHibernate";
+        String rotatedStr = "StrutsHibernateJavaJ2ee";
+        int i = 0;
+        while(i < str.length()) {
+            System.out.println(rotate(str, i));
+            if(rotatedStr.equals(rotate(str, i))) {
+                System.out.println("The strings "+str+" and "+rotatedStr+" are rotated strings of each other");
+                break;
+            }
+            i++;
+        }
+        if(i == str.length())
+            System.out.println("The strings "+str+" and "+rotatedStr+" are not rotated strings of each other");
+    }
+    public static String rotate(String str, int pos) {
+        return str.substring(pos) + str.substring(0, pos);
+    }
+}
